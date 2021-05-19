a7c6101a6b7efb1902b6975f9907e3672f1fcbb3
saman-zdf <samanfyz68@gmail.com>
2021-05-19 19:03:20 +0800

    added some methods to the calorierecorder class and bmi class

--- src/BMI_calculator.rb
+++ src/BMI_calculator.rb
@@ -1,17 +1,29 @@
-# require_relative "./calorie_recorder"
+require 'colorize'
+class Bmi 
+  attr_reader :weight, :height
+  # in this method create two instance variables to get the weight and height of the user
+  def initialize(weight, height)
+    @weight = weight
+    @height = height
+  end
 
-# class Bmi 
-#   attr_reader :weight, :height
-#   # in this method create two instance variables to get the weight and height of the user
-#   def initialize
-#     super name
-#     @weight = weight
-#     @height = height
-#   end
-
-#   # this method will calculate and return user's BMI
-#   def get_bmi
-#     total_bmi = @weight / (height * height)
-#     return total_bmi
-#   end
-# end
+  # this method will calculate and return user's BMI
+  def get_bmi(total_bmi)
+    @total_bmi = total_bmi
+    @total_bmi = @weight / (height * height)
+    # puts @total_bmi
+  end
+  
+  def display
+    if get_bmi(@total_bmi).round(1) >= 18.4 && get_bmi(@total_bmi).round(1) <= 24.9
+      puts "Good news, your BMI is #{get_bmi(@total_bmi).round(1)}, you have healthy weight.".colorize(:green)
+    elsif get_bmi(@total_bmi).round(1) >= 25
+      puts "Your BMI is a bit high #{get_bmi(@total_bmi).round(1)}, you are overwieght".colorize(:red)
+      puts "If you reduce your calorie intake by 500 calories a day, you will be able to lose (450g) in a week".colorize(:red)
+    elsif get_bmi(@total_bmi).round(1) <= 18.3
+      puts "Your BMI is a bit low #{get_bmi(@total_bmi).round(1)}, you are underwieght".colorize(:yellow)
+      puts "If you increase your calorie intake by 500 calories a day, you will be able to gain (450g) in a week.".colorize(:yellow)
+    end
+  end
+    
+end
--- src/calorie_recorder.rb
+++ src/calorie_recorder.rb
@@ -27,79 +27,23 @@ class Calorierecorder
   end
   
   def display
-    if @record == []
-      puts "Your record is empty"
-    elsif @record.length == 1
       @record.each do |item|
         puts "Meal -> #{item[:food]}, #{item[:calorie]} -> calories, Time -> #{item[:time]}"
       end
-    elsif @record >= 2 
-      @record.each do |item|
-        puts "Meal -> #{item[:food]}, #{item[:calorie]} -> calories, Time -> #{item[:time]}"
+  end
+
+  def history
+    if @record.length == 0 
+      puts "Your log is empty."
+      elsif @record.length >= 1 
+      @record.each_with_index do |item, index|
+        puts "#{index += 1}. Meal: #{item[:food]}, #{item[:calorie]} Calories, #{item[:time]} "
       end
-      puts "You have had #{@record.length} inputs."
-    end 
-  end
-end
-
-
-class Bmi < Calorierecorder
-  attr_reader :weight, :height
-  # in this method create two instance variables to get the weight and height of the user
-  def initialize(weight, height)
-    @weight = weight
-    @height = height
-  end
-
-  # this method will calculate and return user's BMI
-  def get_bmi
-    total_bmi = @weight / (height * height)
-    puts total_bmi
-  end
-end
-
-
-loop do 
-  puts "please Enter you name: "
-  name = gets.chomp
-  user = Calorierecorder.new(name)
-  user.welcome
-  input = gets.chomp
-  break if input != "y"
-  option = true
-  while option == true && input == "y"
-    user.option 
-    user_input = gets.chomp.downcase
-    if user_input == "calorie" && option == true
-      puts "Please enter the type of food: "
-      food = gets.chomp
-      puts "Please enter the amount of calories: "
-      calorie = gets.chomp
-      puts "Please enter the time of day:"
-      time = gets.chomp
-      user.get_calorie(food, calorie, time)
-      user.display
-      option == true
-    elsif user_input == "bmi" && option == true
-      puts "Please enter your weight: (kg)"
-      weight = gets.chomp.to_f 
-      puts "Please enter your height:(m),like(1.75)"
-      height = gets.chomp.to_f
-      bmi = Bmi.new(weight, height)
-      bmi.get_bmi
-      option = true
-    elsif user_input == "history" && option == true
-      user.display
-      option = true
+      puts "You have been your meal intakes #{@record.length} times!".colorize(:green)
     end
-    puts "Would you like to continue?(Y/N)"
-    answer = gets.chomp
-    if answer == "y"
-      option = true
-    else answer != Y
-      puts "thank you for using our app #{user.name}, good luck!"
-      option = false
-    end
-
   end
 end
+
+
+
+
