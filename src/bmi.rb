f2c0c6e6d9c66e1654919d9888bb63ce462da700
saman-zdf <samanfyz68@gmail.com>
2021-05-22 14:29:56 +0800

    added some description into the readme file and change some features in calorie file

--- src/bmi.rb
+++ src/bmi.rb
@@ -1,27 +1,27 @@
 require 'colorize'
 class Bmi 
   attr_reader :weight, :height
-  # in this method create two instance variables to get the weight and height of the user
-  def initialize(weight, height)
-    @weight = weight
-    @height = height
-  end
+  # def initialize(weight, height)
+  #   @weight = weight
+  #   @height = height
+  # end
+
 
   # this method will calculate and return user's BMI
-  def get_bmi(total_bmi)
-    @total_bmi = total_bmi
-    @total_bmi = @weight / (height * height)
-    # puts @total_bmi
+  def get_bmi(weight, height)
+    @weight = weight
+    @height = height
+    @weight / (@height * @height)
   end
   # # in this method will display the user's BMI, using conditional statments, if user's BMI in a normal range it shows He/She has a healthy weigh, if not in a normal range, it will alert the user that he either underweight or overweight, and gives some tip to how to lose or gaing weight.
   def display
-    if get_bmi(@total_bmi).round(1) >= 18.4 && get_bmi(@total_bmi).round(1) <= 24.9
-      puts "Good news, your BMI is #{get_bmi(@total_bmi).round(1)}, you have healthy weight.".colorize(:green)
-    elsif get_bmi(@total_bmi).round(1) >= 25
-      puts "Your BMI is a bit high #{get_bmi(@total_bmi).round(1)}, you are overwieght".colorize(:red)
+    if get_bmi(@weight, @height).round(1) >= 18.4 && get_bmi(@weight, @height).round(1) <= 24.9
+      puts "Good news, your BMI is #{get_bmi(@weight, @height).round(1)}, you have healthy weight.".colorize(:green)
+    elsif get_bmi(@weight, @height).round(1) >= 25
+      puts "Your BMI is a bit high #{get_bmi(@weight, @height).round(1)}, you are overwieght".colorize(:red)
       puts "If you reduce your calorie intake by 500 calories a day, you will be able to lose (450g) in a week".colorize(:red)
-    elsif get_bmi(@total_bmi).round(1) <= 18.3
-      puts "Your BMI is a bit low #{get_bmi(@total_bmi).round(1)}, you are underwieght".colorize(:yellow)
+    elsif get_bmi(@weight, @height).round(1) <= 18.3
+      puts "Your BMI is a bit low #{get_bmi(@weight, @height).round(1)}, you are underwieght".colorize(:yellow)
       puts "If you increase your calorie intake by 500 calories a day, you will be able to gain (450g) in a week.".colorize(:yellow)
     end
   end
--- src/calorie_recorder.rb
+++ src/calorie_recorder.rb
@@ -1,15 +1,19 @@
 require 'colorize'
 require 'tty-prompt'
+require 'terminal-table'
 # create a class to get a user name, and data of food and calorie antake
-class Calorierecorder 
+class Calorierecorder < Terminal::Table
   # use attr_reader to be able to read the name 
-  attr_reader :name
+  attr_reader :name, :username, :password
   # in initializa methos which is a private method I passed 1 arguments for name and create two instance variables for name and record, and assign record varriable to an empty array
   def initialize(name)
     @name = name
     @record = []
   end
-# create welcome method to welcome the to this app user
+
+
+
+  # create welcome method to welcome the to this app user
   def welcome 
     puts "Welcome to my calorie recorder app"
     puts "In this app you will be able to record your daily food intake with calorie and time of the day.".colorize(:cyan)
@@ -28,25 +32,57 @@ class Calorierecorder
   end
   # create a display data  method and iterate through my @record array, to display the value of each hash
   def display_data
+    @table = Terminal::Table.new do |t|
+      rows = []
+      t.headings = ['Meal'.colorize(:yellow), 'Calories'.colorize(:yellow), 'Time of the day'.colorize(:yellow)]
       @record.each do |item|
-        puts "Meal -> #{item[:food]}, #{item[:calorie]} -> calories, Time -> #{item[:time]}"
+        t << [item[:food], item[:calorie], item[:time] ] 
+        t << :separator
+        t.style = { :border_top => false, :border_bottom => false, :width => 80,:border_x => "=".colorize(:blue),:border_i => "*".colorize(:red)}
       end
+    end
+    puts @table
   end
 # create a history method to show the history of records, used the conditinal staement if the sistory is empty it will display your log is empty, io
   def history
     if @record.length == 0 
       puts "Your log is empty."
       elsif @record.length >= 1 
-      @record.each_with_index do |item, index|
-        puts "#{index += 1}. Meal: #{item[:food]}, #{item[:calorie]} Calories, #{item[:time]} "
-      end
-      sum_calorie = @record.map { |x| x[:calorie] }.sum
-      puts "your meal intake are #{@record.length} times, and have #{sum_calorie} calories in total.".colorize(:green)
+        puts ''
+      @table = Terminal::Table.new do |t|
+          rows = []
+          t.headings = ['Meal', 'Calories', 'Time of the day']
+        @record.each do |item|
+          t << [item[:food], item[:calorie], item[:time] ] 
+          t << :separator
+          t.style = { :border_top => false, :border_bottom => false, :width => 80}
+        end
     end
+      puts @table
+      sum_calorie = @record.map { |x| x[:calorie] }.sum
+      puts "you have had #{@record.length} meals, and  #{sum_calorie} calories in total.".colorize(:green)
+    end
+  end
+
+
+  def display_bye
+    puts "Thank you for visiting us, please caome back again".colorize(:light_magenta)
+      puts ""
+        puts "$$$$$$$\ $$\     $$\ $$$$$$$$\ 
+$$  __$$\\$$\   $$  |$$  _____|
+$$ |  $$ |\$$\ $$  / $$ |      
+$$$$$$$\ | \$$$$  /  $$$$$\    
+$$  __$$\   \$$  /   $$  __|   
+$$ |  $$ |   $$ |    $$ |      
+$$$$$$$  |   $$ |    $$$$$$$$\ 
+\_______/    \__|    \________|
+          ".colorize(:yellow)
   end
 end
 
-
-
-
-
+# user = Calorierecorder.new("saman")
+# user.get_data("Tuna", 300, "Morning")
+# user.get_data("pizza", 400, "Midday")
+# user.get_data("chicken", 500, "Afternoon")
+# user.get_data("beef", 900, "Night")
+# user.display_data
