8d0b9a2edd40cdbe196f062b9584ada11a6745eb
saman-zdf <samanfyz68@gmail.com>
2021-05-23 12:37:04 +0800

    refactor the user_interface codes  and created new class for username and password

--- src/calorie_recorder.rb
+++ src/calorie_recorder.rb
@@ -10,24 +10,29 @@ class Calorierecorder < Terminal::Table
     @name = name
     @record = []
   end
-
   # create welcome method to welcome the to this app user
   def welcome 
     puts "Welcome to my calorie recorder app"
     puts "In this app you will be able to record your daily food intake with calorie and time of the day.".colorize(:cyan)
     puts "Please just follow the instruction and you will be able to use this app easily.".colorize(:cyan)
   end 
-
-  def option 
-    puts "#{@name}:".colorize(:cyan)
-    puts "would you like to record your calorie intake, calculate your BMI or check the calorie history?".colorize(:cyan)
-  end
-
 # create the get data method and pass three arguments, I store the variables as a value to the hash and then append it to my @record variables
   def get_data(type, nmuber_of_cal, time_of_day)
     total = {food: type, calorie: nmuber_of_cal, time: time_of_day}
     @record << total
   end
+
+    def calorie_recorder
+      puts "Please Enter the type of the food"
+      type = gets.chomp
+      puts "Please enter the number of calories"
+      nmuber_of_cal = gets.chomp.to_i
+      puts "Please enter the time of the day"
+      time = gets.chomp
+      get_data(type, nmuber_of_cal, time)
+      system("clear")
+      display_data
+    end
   # create a display data  method and iterate through my @record array, to display the value of each hash
   def display_data
     @table = Terminal::Table.new do |t|
@@ -49,16 +54,16 @@ class Calorierecorder < Terminal::Table
         puts ''
       @table = Terminal::Table.new do |t|
           rows = []
-          t.headings = ['Meal', 'Calories', 'Time of the day']
+          t.headings = ['Meal'.colorize(:yellow), 'Calories'.colorize(:yellow), 'Time of the day'.colorize(:yellow)]
         @record.each do |item|
           t << [item[:food], item[:calorie], item[:time] ] 
           t << :separator
-          t.style = { :border_top => false, :border_bottom => false, :width => 80}
+          t.style = { :border_top => false, :border_bottom => false, :width => 80,:border_x => "=".colorize(:blue),:border_i => "*".colorize(:red)}
         end
     end
       puts @table
       sum_calorie = @record.map { |x| x[:calorie] }.sum
-      puts "you have had #{@record.length} meals, and  #{sum_calorie} calories in total.".colorize(:green)
+      puts "you have had #{@record.length} meals, and #{sum_calorie} calories in total.".colorize(:green)
     end
   end
 end
--- src/user_interface.rb
+++ src/user_interface.rb
@@ -3,137 +3,65 @@ require 'tty-prompt'
 require_relative "bmi.rb"
 require_relative "calorie_recorder.rb"
 require_relative "greet.rb"
-# require "test.yml"
+require_relative "sign_up.rb"
 require 'terminal-table'
+# create a new object from tty-prompt 
 prompt = TTY::Prompt.new
+# createin a new object from UserAccount class
+user_account = UserAccount.new
+# createin a new object from Greet class
+greet = Greet.new
+# createin a new object from Bmi class
+bmi = Bmi.new(10, 5)
 
 
-# in this loop the user have st of option, like sign up, create username and password, and login,
-# and prmopt the user to choose if user to update calorie intake or calulate bmi or see the history of calorie intake 
-loop do 
-  greet = Greet.new
-  greet.welcome_msg
-  # this code will ask if user wants to sign-up or exit
-  user = Calorierecorder.new("saman")
-  question = prompt.select("Would like to Sign-Up or Exit", %w(Sign-up Exit))
-
-  if question == "Sign-up"
-    system("clear")
-  elsif question == "Exit"
-    greet.bye_msg
-    break
-  end
-
-  continue = true
-  # this while loop is for creating username and password,
-  # when user create the username user get promp if user sure to choose a right username, if yse continue, if no user can type new username
-  while continue
-    puts "For sign-up you need to create an account, let's start".colorize(:cyan)
-    username1 = prompt.ask("Please create a username: ")
-    system("clear")
-    answer = prompt.ask("You created #{username1}, Are you sure?(Y/N)")
-    system("clear")
-    if answer == "y"
-      break
-    else
-      continue = true
-    end
-  end
-
-  continue_2 = true
-  # this while is for user to create a password and to retype the password, if user retype the wrong password, user will get prompt "Wrong combination, try to create a password again" 
-  while continue_2
-    password = prompt.mask("Please create a Password: ")
-    system("clear")
-    password_2 = prompt.mask("Please retype your password: ")
-    system("clear")
-    if password == password_2
-      break
-      system("clear")
-      elsif password != password_2
-        puts "Wrong combination, please try to create a password again ".colorize(:red)
-        continue_2 = true
-    end
-  end
-
-  # in this test create a new object form Calorierecoder 
-  user = Calorierecorder.new(username1)
-  # display a welcome meesage to the user
-  user.welcome
-
-  # in this while loop ask the user to log in by putting username and password
-    continue_3 = true
-  while continue_3 == true  
-      puts "To use this app enter your username and password to log-in"
-      username = prompt.ask("Username:")
-      userpass = prompt.mask("Password: ")
-    if username == username1 && userpass == password
-      continue_3 = false
-      system('clear')
-      break
-    else
-      puts "Wrong credentials, please try agin"
-      continue_3 = true
-    end
-  end
-
-  # in this while loop we will display ask the user what kind option there are to choose
-  option = true
-  while option == true
-    user.option
-    display_option = prompt.select("You can choose one of the following options", %w(RecordCalorie BMI History Exit))
-    system("clear ")
-
-    # if the user chooses to record the calorie intake, it will ask to input the typr meal, number of calorie and time of the day
-    if display_option == "RecordCalorie" && option == true
-      puts "Please enter the type of the food: ".colorize(:yellow)
-      food = gets.chomp
-      puts "Please enter the number of calories: ".colorize(:yellow)
-      calorie = gets.chomp.to_i
-      puts  "Please salect time of the day: (Morning, Afternoon, Night)".colorize(:yellow)
-      time = gets.chomp
-      user.get_data(food, calorie, time)
-      system("clear")
-      user.display_data
-      option = true
-
-      # if the user chooses to check the BMI, it will ask go weight and for the height and return the bmi 
-    elsif display_option == "BMI" && option == true
-      puts "Please enter your weight(kg)".colorize(:green)
-      weight = gets.chomp.to_f
-      puts "Please enter your height(m), like(1.82)".colorize(:green)
-      height = gets.chomp.to_f
-      bmi = Bmi.new
-      bmi.get_bmi(weight, height)
-      system("clear")
-      bmi.display
-      option = true
-
-      # if the user chooses to see the history it will disply the history of meal and calorie reords and total of calorie intake
-    elsif display_option == "History" && option == true
-      user.history
-      option = true
-    elsif display_option == "Exit" && option == true
+status = true
+while status == true
+    greet.welcome_msg
+    if user_account.sign_up == "Sign-Up" 
+      user_account.get_useranme
+      user_account.get_password
+    elsif user_account.sign_up == "Exit" 
       greet.bye_msg
-      option = false
       exit
+      status = false
     end
-
-    # in this while loop it will ask the user if he wants to check options again, if no it will exit the loops
-    option_2 = true
-    while option_2 == true
-      answer = prompt.select("Would you like to see the options again?", %w(Yes No))
-      if answer == "Yes"
-        option_2 = false
-        system("clear")
-      else answer == "No"
-      greet.bye_msg     
-        exit
-      end
+  user = Calorierecorder.new(user_account.username)
+  system("clear")
+  user_account.log_in
+  system("clear")
+  user.welcome
+  continue = true 
+  while continue == true
+    puts "would you like to record your calorie intake, calculate your BMI or check the calorie history?".colorize(:cyan)
+    selection = prompt.select("You can choose one of the following options", %w(RecordCalorie BMI History Exit))
+    system("clear")
+    if selection == "RecordCalorie"
+      user.calorie_recorder
+      continue = true
+    elsif selection == "BMI"
+      bmi.display
+      continue = true
+    elsif selection == "History"
+      user.history
+      continue = true
+    elsif selection == "Exit"
+      greet.bye_msg
+      status = true
+      break
+    end
+    input = prompt.select("Would you like to see the option again?", %w(Yes NO))
+    if input == "Yes"
+      continue = true
+      system("clear")
+    elsif input == "NO"
+      continue = false
+      greet.bye_msg
+      break
+    end
   end
+  status = false
 end
-end
-
 
 
 
